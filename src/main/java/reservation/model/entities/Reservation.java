1f881837234d643c7cc128d4fdd63d740bd2ea25
Lucas Figueiredo <lukas.liberato14@gmail.com>
2023-12-14 09:22:32 -0300

    Versão ruim

---
+++ src/main/java/reservation/model/entities/Reservation.java
@@ -0,0 +1,61 @@
+package reservation.model.entities;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    private Integer roomNumber;
+
+    private Date checkIn;
+
+    private Date checkOUt;
+
+    public Reservation(Integer roomNumber, Date checkIn, Date checkOUt) {
+        this.roomNumber = roomNumber;
+        this.checkIn = checkIn;
+        this.checkOUt = checkOUt;
+    }
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public Date getCheckOUt() {
+        return checkOUt;
+    }
+
+    public long duration() {
+        long diff = checkOUt.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOUt = checkOut;
+    }
+
+    @Override
+    public String toString() {
+        return "Room "
+                + roomNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkOUt)
+                + ", "
+                + duration()
+                + " nights";
+    }
+}
